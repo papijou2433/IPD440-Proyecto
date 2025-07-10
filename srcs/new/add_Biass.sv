659cc963e42235349373e861d6ee37e7a077efaf
Sebastian Sanchez <sebastian.sanchezp@usm.cl>
2025-07-10 13:53:13 -0400

    udpated add_Biass

--- srcs/new/add_Biass.sv
+++ srcs/new/add_Biass.sv
@@ -4,14 +4,14 @@ module add_Bias(
     input logic[34:0] adder_res,
     output logic[34:0] Bias_res
 );
-logic[34:0] Bias;
+logic[33:0] Bias;
 always_comb begin
     case(Bias_sel)
-        2'b00: Bias = 35'h959B3D08; // Example bias value for channel 0
-        2'b01: Bias = 35'h3FFFFFFBE ;
-        2'b10: Bias = 35'hEE00D1B7 ;
-        2'b11: Bias = 35'h1A305532 ;
-        default:Bias= 35'h3353F7CF ;
+        2'b00: Bias = 34'sh2566CF42; // Example bias value for channel 0
+        2'b01: Bias = 34'shEE00D1B7 ;
+        2'b10: Bias = 34'sh1A305532 ;
+        2'b11: Bias = 34'sh3353F7CF ;
+        default:Bias= 34'sh3353F7CF ;
     endcase
     Bias_res = Bias + adder_res;
 end
